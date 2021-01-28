0d7f1ee3f95057dc3166a9696d968d0cc6548ba5
Prasad Rotti <prasad.rotti287@gmail.com>
2021-01-28 13:08:27 +0530

    Added mux

--- commands.sh
+++ commands.sh
@@ -3,6 +3,12 @@ curl http://localhost:8000/greet
 curl --header "Content-Type: application/json" http://localhost:8000/customers
 curl --header "Content-Type: application/xml" http://localhost:8000/customers
 # Not working: curl --header "Content-Type: text/csv" http://localhost:8000/customers 
+curl --header "Content-Type: application/xml" http://localhost:8000/customers/123
+
+# POSTing with curl’s -d option will include a default header that looks like: Content-Type: application/x-www-form-urlencoded
+curl -X POST http://localhost:8000/customers
+curl -d "user=user1&pass=abcd" -X POST http://localhost:8000/customers 
+
 
 # Kill process
 lsof -i :8000 <port number>
